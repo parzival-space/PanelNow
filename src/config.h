a1c4184b6acbe50a3922a5f428c3b52fd31ad784
Malte Linke (Parzival) <me@parzival.space>
2024-09-08 01:23:21 +0200

    Current dev state

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#include "Arduino.h"
+
+// serial monitor baud rate
+#define SERIAL_BAUD 115200
+
+// bridge device mac address
+uint8_t BRIDGE_MAC[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
+
+// panel device mac address
+uint8_t PANEL_MAC[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
--- src/main-bridge.cpp
+++ src/main-bridge.cpp
@@ -1,11 +1,63 @@
 #include "Arduino.h"
+#include "config.h"
+#include "ESP8266WiFi.h"
+#include "espnow.h"
+
+void onDataSent(uint8_t *mac_addr, uint8_t status)
+{
+    Serial.print("Last Packet Send Status: ");
+    if (status == 0)
+    {
+        Serial.println("Delivery success");
+    }
+    else
+    {
+        Serial.println("Delivery fail");
+    }
+}
+
+void onDataRecv(uint8_t *mac, uint8_t *data, uint8_t len)
+{
+    Serial.print("Data received: ");
+    for (int i = 0; i < len; i++)
+    {
+        Serial.print((char)data[i]);
+    }
+    Serial.println();
+}
 
 void setup()
 {
+    Serial.begin(SERIAL_BAUD);
+    Serial.println("Starting...");
+
+    // start Wi-Fi and set mac
+    WiFi.mode(WIFI_STA);
+    const String oldMac = WiFi.macAddress();
+    wifi_set_macaddr(STATION_IF, BRIDGE_MAC);
+    Serial.println("Updated device MAC: " + oldMac + " -> " + WiFi.macAddress());
+
+    // init ESP-NOW
+    Serial.println("Initializing ESP-NOW...");
+    if (esp_now_init() != 0)
+    {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    // complete esp now setup
+    esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+    esp_now_register_send_cb(onDataSent);
+
+    // register peer
+    esp_now_add_peer(PANEL_MAC, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
+
+    // register callback for incoming data
+    esp_now_register_recv_cb(onDataRecv);
 
 }
 
 void loop()
 {
-
+    delay(1000);
 }
